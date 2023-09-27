92094401c95a7ef7dbe3317cdbeb31ba2a21ed9c
Patrick Nguyen <patar.nguyen@gmail.com>
2023-09-27 10:50:32 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Patrick Nguyen on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
